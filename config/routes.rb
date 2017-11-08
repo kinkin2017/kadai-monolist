3fb352a1988afe7def053fd4aff735753a0b5973
kin <kinsa.ka@g.softbank.co.jp>
2017-11-08 16:42:30 +0000

    rakuten api

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   root to: 'toppages#index'
   
   get 'login', to: 'sessions#new'
-  get 'login', to: 'sessions#create'
-  get 'logout', to: 'sessions#destroy'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   resources :users, only: [:show, :new, :create]
+  
+  resources :items, only: [:new]
 end
